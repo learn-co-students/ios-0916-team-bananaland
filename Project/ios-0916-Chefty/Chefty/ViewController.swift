7a7f0fdb3f8e1ecd081f67548cd673db107abaa9
arb1nsnmgl <arvinsanmiguel@gmail.com>
2016-11-16 13:21:41 -0500

    Created Project folder

---
+++ Project/ios-0916-Chefty/Chefty/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chefty
+//
+//  Created by Arvin San Miguel on 11/16/16.
+//  Copyright © 2016 com.AppRising.SML. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
