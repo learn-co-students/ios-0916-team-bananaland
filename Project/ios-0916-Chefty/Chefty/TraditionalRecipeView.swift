154cc1a6ff5db263461e606e39cb955fc5cb3dd5
jacqschweiger <jacqschweiger@gmail.com>
2016-11-17 13:57:12 -0500

    added trad recipe view file

---
+++ Project/ios-0916-Chefty/Chefty/TraditionalRecipeView.swift
@@ -0,0 +1,9 @@
+//
+//  TraditionRecipeView.swift
+//  Chefty
+//
+//  Created by Jacqueline Minneman on 11/17/16.
+//  Copyright © 2016 com.AppRising.SML. All rights reserved.
+//
+
+import Foundation
