bb2489437583c119971b7221dbeaf310e8ca04f1
jacqschweiger <jacqschweiger@gmail.com>
2016-11-18 11:51:54 -0500

    changes

--- Project/ios-0916-Chefty/Chefty/TraditionalRecipeView.swift
+++ Project/ios-0916-Chefty/Chefty/TraditionalRecipeView.swift
@@ -21,8 +21,6 @@ class TraditionalRecipeView: UIView {
         let myImageName = "applePie"
         let myImage = UIImage(named: myImageName)
         let myImageView = UIImageView(image: myImage!)
-        
-        // add the image
         self.addSubview(myImageView)
         
         // constrain the image
@@ -31,26 +29,133 @@ class TraditionalRecipeView: UIView {
         myImageView.translatesAutoresizingMaskIntoConstraints = false
         
         
-        //TITLE
+        //RECIPE TITLE
         //create title label
         let titleLabel = UILabel()
         titleLabel.text = "Recipe Title"
-        titleLabel.font = titleLabel.font.withSize(40)
+        titleLabel.font = titleLabel.font.withSize(30)
         titleLabel.textAlignment = .center
-        titleLabel.backgroundColor = UIColor.red
-        
-        
-        //add the title label
         self.addSubview(titleLabel)
         
         // constrain the image
         titleLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
         titleLabel.topAnchor.constraint(equalTo: myImageView.bottomAnchor).isActive = true
         titleLabel.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
-        titleLabel.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        titleLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         
         
+        //SERVING SIZE AND ESTIMATED TIME INFO
+        //create labels
+        let servingSizeLabel = UILabel()
+        servingSizeLabel.text = "Serving Size: 4-6"
+        servingSizeLabel.font = titleLabel.font.withSize(20)
+        servingSizeLabel.textAlignment = .left
+        
+        let durationLabel = UILabel()
+        durationLabel.text = "Estimated Total Time: 26 minutes"
+        durationLabel.font = titleLabel.font.withSize(20)
+        durationLabel.textAlignment = .left
+        self.addSubview(servingSizeLabel)
+        self.addSubview(durationLabel)
+        
+        // constrain labels
+        servingSizeLabel.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 10).isActive = true
+        servingSizeLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor).isActive = true
+        servingSizeLabel.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
+        servingSizeLabel.heightAnchor.constraint(equalToConstant: 24).isActive = true
+        servingSizeLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        durationLabel.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 10).isActive = true
+        durationLabel.topAnchor.constraint(equalTo: servingSizeLabel.bottomAnchor).isActive = true
+        durationLabel.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
+        durationLabel.heightAnchor.constraint(equalToConstant: 24).isActive = true
+        durationLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        //INGREDIENTS 
+        
+        //LABEL
+        //create label
+        let ingredientsLabel = UILabel()
+        ingredientsLabel.text = "Ingredients"
+        ingredientsLabel.font = titleLabel.font.withSize(16)
+        ingredientsLabel.textAlignment = .left
+        self.addSubview(ingredientsLabel)
+        
+        //constrain label
+        ingredientsLabel.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 10).isActive = true
+        ingredientsLabel.topAnchor.constraint(equalTo: durationLabel.bottomAnchor, constant: 10).isActive = true
+        ingredientsLabel.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
+        ingredientsLabel.heightAnchor.constraint(equalToConstant: 18).isActive = true
+        ingredientsLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        
+        //TEXT BOX
+        //create textbox
+        let ingredientsText = UITextView()
+        ingredientsText.text = "Lorem ipsum dolor sit amet \nconsectetur adipiscing elit \nsed do eiusmod tempor \nincididunt ut labore \net dolore magna aliqua"
+        ingredientsText.font = titleLabel.font.withSize(14)
+        ingredientsText.textAlignment = .left
+        self.addSubview(ingredientsText)
+        
+        // constrain textbox
+        let ingredientsContentSize = ingredientsText.sizeThatFits(ingredientsText.bounds.size)
+        var ingredientsFrame = ingredientsText.frame
+        ingredientsFrame.size.height = ingredientsContentSize.height
+        ingredientsText.frame = ingredientsFrame
+        
+        ingredientsText.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 10).isActive = true
+        ingredientsText.topAnchor.constraint(equalTo: ingredientsLabel.bottomAnchor).isActive = true
+        ingredientsText.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
+        ingredientsText.heightAnchor.constraint(equalToConstant: ingredientsText.frame.size.height).isActive = true
+        ingredientsText.translatesAutoresizingMaskIntoConstraints = false
+        ingredientsText.isScrollEnabled = false
+        
+        
+        
+        //STEPS 
+        
+        //LABEL
+        //create label
+        let stepsLabel = UILabel()
+        stepsLabel.text = "Steps"
+        stepsLabel.font = titleLabel.font.withSize(16)
+        stepsLabel.textAlignment = .left
+        self.addSubview(stepsLabel)
+        
+        //constrain label
+        stepsLabel.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 10).isActive = true
+        stepsLabel.topAnchor.constraint(equalTo: ingredientsText.bottomAnchor, constant: 10).isActive = true
+        stepsLabel.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
+        stepsLabel.heightAnchor.constraint(equalToConstant: 18).isActive = true
+        stepsLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        //TEXT BOX
+        //create textbox
+        let stepsText = UITextView()
+        stepsText.text = "Lorem ipsum dolor sit amet \nconsectetur adipiscing elit \nsed do eiusmod tempor \nincididunt ut labore \net dolore magna aliqua\nSed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque \nlaudantium totam rem aperiam eaque ipsa quae ab \nillo inventore veritatis et quasi \narchitecto beatae vitae dicta sunt explicabo\n Nemo enim ipsam voluptatem \nquia voluptas sit aspernatur aut odit aut fugit \nsed quia consequuntur magni \ndolores eos qui ratione voluptatem \nsequi nesciunt \nNeque porro quisquam est \nqui dolorem ipsum quia dolor sit amet \nconsectetur adipisci velit"
+        stepsText.font = titleLabel.font.withSize(14)
+        stepsText.textAlignment = .left
+        self.addSubview(stepsText)
+        
+        // constrain textbox
+        let stepsContentSize = stepsText.sizeThatFits(stepsText.bounds.size)
+        var stepsFrame = stepsText.frame
+        stepsFrame.size.height = stepsContentSize.height
+        stepsText.frame = stepsFrame
+        
+        stepsText.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 10).isActive = true
+        stepsText.topAnchor.constraint(equalTo: stepsLabel.bottomAnchor).isActive = true
+        stepsText.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
+        stepsText.heightAnchor.constraint(equalToConstant: ingredientsText.frame.size.height).isActive = true
+        stepsText.translatesAutoresizingMaskIntoConstraints = false
+        stepsText.isScrollEnabled = false
+        
+
+        
+        
+
     }
     
     required init?(coder aDecoder: NSCoder) {
