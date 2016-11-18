423e9cb95ff6367d4cd89f67ec9b19fafa51eea1
arb1nsnmgl <arvinsanmiguel@gmail.com>
2016-11-18 10:48:49 -0500

    Add gradient layer

--- Project/ios-0916-Chefty/Chefty/HomePageViewController.swift
+++ Project/ios-0916-Chefty/Chefty/HomePageViewController.swift
@@ -14,6 +14,9 @@ class HomePageViewController: UIViewController, UICollectionViewDelegate, UIColl
     var recipeImages = [#imageLiteral(resourceName: "moroccanChicken"), #imageLiteral(resourceName: "dijonChickenBreasts"), #imageLiteral(resourceName: "sweetPotatoFries"), #imageLiteral(resourceName: "beefBroccoliStirFry"), #imageLiteral(resourceName: "peachCobbler"),
                         #imageLiteral(resourceName: "snickerdoodleCookies"),#imageLiteral(resourceName: "applePie"), #imageLiteral(resourceName: "BarbequedDeviledEggs"), #imageLiteral(resourceName: "AuthenticItalianMeatballs"), #imageLiteral(resourceName: "marinatedCheeseAppetizer"), #imageLiteral(resourceName: "choppedSaladAppetizerShells"), #imageLiteral(resourceName: "balsamicBrusselSprouts"), #imageLiteral(resourceName: "blackBeanCouscousSalad"), #imageLiteral(resourceName: "RoastedGreenBeans"), #imageLiteral(resourceName: "yummyBakedPotatoSkins")]
     
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -31,7 +34,6 @@ class HomePageViewController: UIViewController, UICollectionViewDelegate, UIColl
         collectionView.dataSource = self
         collectionView.backgroundColor = UIColor.clear
         collectionView.register(recipeCollectionViewCell.self, forCellWithReuseIdentifier: "recipeCell")
-        //collectionView.setCollectionViewLayout(layout, animated: <#T##Bool#>)
         view.addSubview(collectionView)
     }
     
@@ -44,6 +46,30 @@ class HomePageViewController: UIViewController, UICollectionViewDelegate, UIColl
         cell.imageView.image = recipeImages[indexPath.row]
         cell.backgroundColor = UIColor.white
         return cell
+    }
+    
+     func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        
+        let interval = Double(indexPath.row)
+        
+        if (collectionView.frame.intersects(cell.frame)) {
+            cell.alpha = 0.0
+            cell.center.y = cell.center.y - 30
+            
+            UIView.animate(withDuration: 0.7 + (interval * 0.05), animations: {
+                cell.alpha = 1.0
+                cell.center.y += 30
+            })
+        }
+        
+        cell.alpha = 0.0
+        cell.center.y = cell.center.y + 20
+        
+        UIView.animate(withDuration: 0.6 + (interval * 0.05), animations: {
+            cell.alpha = 1.0
+            cell.center.y -= 20
+        })
+        
     }
    
     
--- Project/ios-0916-Chefty/Chefty/recipeCollectionViewCell.swift
+++ Project/ios-0916-Chefty/Chefty/recipeCollectionViewCell.swift
@@ -11,21 +11,26 @@ import UIKit
 class recipeCollectionViewCell: UICollectionViewCell {
  
     var imageView : UIImageView!
+    var gradientView : UIView!
+    var recipeLabel : UILabel!
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         commonInit()
+        insertGradientLayer()
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         commonInit()
+        insertGradientLayer()
     }
     
     private func commonInit() {
         
+        //layout ImageView
         imageView = UIImageView()
-        imageView.contentMode = .redraw
+        imageView.contentMode = .scaleAspectFill
         imageView.layer.cornerRadius = 10.0
         imageView.layer.masksToBounds = true
         self.contentView.addSubview(imageView)
@@ -36,6 +41,27 @@ class recipeCollectionViewCell: UICollectionViewCell {
         imageView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor).isActive = true
         imageView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor).isActive = true
         
+        gradientView = UIView()
+        gradientView.frame = self.contentView.frame
+        gradientView.layer.cornerRadius = 10.0
+        gradientView.layer.masksToBounds = true
+        self.contentView.addSubview(gradientView)
+        
     }
+    
+    private func insertGradientLayer() {
+        
+        let gradient = CAGradientLayer()
+        let blackColor = UIColor.black.withAlphaComponent(1.0)
+        
+        gradient.frame = self.contentView.frame
+        gradient.colors = [UIColor.clear.cgColor, blackColor.cgColor]
+        gradient.locations = [0.5, 1.0]
+        gradient.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradient.endPoint = CGPoint(x: 0.0, y: 1.0)
+        self.gradientView.layer.insertSublayer(gradient, at: 0)
+        
+    }
+    
     
 }
