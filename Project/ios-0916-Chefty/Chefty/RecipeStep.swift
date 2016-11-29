fadf135b6bb4dc2d018cb9986ce0052a9b988fda
jacqschweiger <jacqschweiger@gmail.com>
2016-11-29 14:45:54 -0500

    initialized RecipeStep with duration and timetostart as ints

--- Project/ios-0916-Chefty/Chefty/RecipeStep.swift
+++ Project/ios-0916-Chefty/Chefty/RecipeStep.swift
@@ -13,36 +13,53 @@ class RecipeStep {
     
     var recipeID: String?
     var stepNumber: Int?
-    //var duration: Date?
-    //var timeToStart: Date?
     var fullAttentionRequired: Bool?
     var stepTitle: String?
     var procedure: String?
     var ingredients: [String]?
     
+    var duration: Int?
+    var timeToStart: Int?
+    
     
     init(dict: [String: Any]) {
         self.recipeID = dict["recipe"] as! String?
         self.stepNumber = Int((dict["step"] as! String?)!)
-        
-//        let durationDouble = Double(dict["duration"] as! String?)!)
-//        self.duration = Date(durationDouble)
-        
-//        let durationString = dict["duration"] as! String?
-//        let dayTimePeriodFormatter = DateFormatter()
-//        dayTimePeriodFormatter.dateFormat = "HH:mm:ss"
-//        let dateString = dayTimePeriodFormatter.string(from: durationString as! Date)
-        
-        
-        //self.duration = Date(timeIntervalSince1970: durationDouble!)
-        
-//        let timeToStartDouble = Double(((dict["timeToStart"]) as! String?)!)
-//        self.timeToStart = Date(timeIntervalSince1970: timeToStartDouble!)
-        
         self.fullAttentionRequired = Bool(((dict["fullAttentionRequired"] as! String?)!))
         self.stepTitle = dict["stepTitle"] as! String?
         self.procedure = dict["procedure"] as! String?
         self.ingredients = dict["ingredients"] as! [String]?
+
+        let durationSeconds = convertStringToMinutes(input: (dict["duration"] as! String?)!)
+        self.duration = durationSeconds
+        
+        let timeToStartSeconds = convertStringToMinutes(input: (dict["timeToStart"] as! String?)!)
+        self.timeToStart = timeToStartSeconds
+        
     }
+}
+
+extension RecipeStep {
+    
+    func convertStringToMinutes(input: String) -> Int {
+        let separatedNum = input.components(separatedBy: ":")
+        let handleMinutesOnly = Int(separatedNum[0])
+        let handleHours = Int(separatedNum[0])
+        let handleMinutesWithHours = Int(separatedNum[1])
+        var totalMinutes: Int = 0
+        
+        switch separatedNum.count {
+        case 2:
+            totalMinutes += handleMinutesOnly!
+        case 3:
+            totalMinutes += ((handleHours! * 60) + (handleMinutesWithHours!))
+        default:
+            print("error")
+        }
+        
+        return totalMinutes
+    }
+    
+    
     
 }
