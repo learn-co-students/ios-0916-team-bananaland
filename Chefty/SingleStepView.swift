9b773a038ac716757cbf49346bae0db69bc9d37f
ptangen <ptangen@ptangen.com>
2016-12-02 18:39:49 -0500

    Make a small image of the recipe available.

--- Chefty/SingleStepView.swift
+++ Chefty/SingleStepView.swift
@@ -22,41 +22,41 @@ class SingleStepView: UIView {
         
             // print the content of the requested recipe
 
-//            for recipe in self.store.recipes {
-//                if "apple-pie" == recipe.id {
-//                    let allSteps = recipe.step?.allObjects as! [Steps]
-//                    for step in allSteps {
-//                        print("\n")
-//                        print("stepTitle: \(step.stepTitle!)")
-//                        print("stepNumber: \(step.stepNumber)")
-//                        print("timeToStart: \(step.timeToStart!)")
-//                        print("duration: \(step.duration!)")
-//                        print("fullAttentionRequired: \(step.fullAttentionRequired)")
-//                        print("procedure: \(step.procedure!)")
-//                        if let ingredientsAny = step.ingredient {
-//                            for ingredientAny in ingredientsAny {
-//                                let ingredient = ingredientAny as? Ingredient;
-//                                if let ingredientUnwrapped = ingredient {
-//                                    print("ingredientDescription: \(ingredientUnwrapped.ingredientDescription)")
-//                                    print("ingredientIsChecked: \(ingredientUnwrapped.isChecked)")
-//                                }
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//        }
+            for recipe in self.store.recipes {
+                if "apple-pie" == recipe.id {
+                    print(recipe.imageURLSmall)
+                    let allSteps = recipe.step?.allObjects as! [Steps]
+                    for step in allSteps {
+                        print("\n")
+                        print("stepTitle: \(step.stepTitle!)")
+                        print("stepNumber: \(step.stepNumber)")
+                        print("timeToStart: \(step.timeToStart)")
+                        print("duration: \(step.duration)")
+                        print("fullAttentionRequired: \(step.fullAttentionRequired)")
+                        print("procedure: \(step.procedure!)")
+                        if let ingredientsAny = step.ingredient {
+                            for ingredientAny in ingredientsAny {
+                                let ingredient = ingredientAny as? Ingredient;
+                                if let ingredientUnwrapped = ingredient {
+                                    print("ingredientDescription: \(ingredientUnwrapped.ingredientDescription)")
+                                    print("ingredientIsChecked: \(ingredientUnwrapped.isChecked)")
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         
-        //  build an array of recipeSteps
-//          let stepsFromRecipe1:[Steps] = store.recipes.first!.step!.allObjects as! [Steps]
-//          let stepsFromRecipe2:[Steps] = store.recipes.last!.step!.allObjects as! [Steps]
-//          let stepsFromBothRecipes = stepsFromRecipe1 + stepsFromRecipe2
-//        
-//          print("stepsFromBothRecipes.count: \(stepsFromBothRecipes.count)")
-//        
-//          print("procedureFromStep: \(stepsFromBothRecipes.first?.procedure)")
-//        
-//          print("step's recipe: \(stepsFromBothRecipes.first?.recipe?.id)")
+          //build an array of recipeSteps
+          let stepsFromRecipe1:[Steps] = self.store.recipes.first!.step!.allObjects as! [Steps]
+          let stepsFromRecipe2:[Steps] = self.store.recipes.last!.step!.allObjects as! [Steps]
+          let stepsFromBothRecipes = stepsFromRecipe1 + stepsFromRecipe2
+        
+          print("stepsFromBothRecipes.count: \(stepsFromBothRecipes.count)")
+        
+          print("procedureFromStep: \(stepsFromBothRecipes.first?.procedure)")
+        
+          print("step's recipe: \(stepsFromBothRecipes.first?.recipe?.id)")
         
             
             // get the steps
