5617338be217ab1ce31047f150d93f331e3f5aa8
ptangen <ptangen@ptangen.com>
2016-12-12 11:57:16 -0500

    Add tests to validate steps and ingredients are added properly.

--- CheftyTests/GeneralTests.swift
+++ CheftyTests/GeneralTests.swift
@@ -30,7 +30,7 @@ class APITests: XCTestCase {
         XCTAssertNotNil(self.store.recipes[0].servings)
     }
     
-    func testVerifyRecipeProperties() {
+    func testRecipeProperties() {
         
         var recipeForTesting: Recipe?
         for recipe in store.recipes {
@@ -44,17 +44,62 @@ class APITests: XCTestCase {
         if let recipeForTesting = recipeForTesting {
             XCTAssert(recipeForTesting.servings == "6-8")
         }
+    }
+    
+    func testRecipeSelected() {
         
-        // set the recipe as selected, add to recipesSelected array
-        if let recipeForTesting = recipeForTesting {
-            store.setRecipeSelected(recipe: recipeForTesting)
+        var recipeForTesting: Recipe?
+        for recipe in store.recipes {
+            if recipe.id == "apple-pie" {
+                recipeForTesting = recipe
+                break
+            }
         }
         
-        // test for the properties
-        if let recipeForTesting = recipeForTesting {
-            XCTAssert(recipeForTesting.selected == true)
-            XCTAssert(store.recipesSelected.count == 1)
+        // determine if the recipeSelected array can be populated.
+        if self.store.recipesSelected.count > 0 {
+            XCTAssert(self.store.recipesSelected.count > 0)
+        } else {
+            // we dont have a selected recipe to try to select one and then unselect it
+            if let recipeForTesting = recipeForTesting {
+                store.setRecipeSelected(recipe: recipeForTesting)
+                XCTAssert(recipeForTesting.selected == true)
+                XCTAssert(store.recipesSelected.count == 1) // selected one recipe, so value should be 1
+                store.setRecipeUnselected(recipe: recipeForTesting)
+                XCTAssert(recipeForTesting.selected == false)
+                XCTAssert(store.recipesSelected.count == 0) // unseelcted the only recipe, value should be 0
+            }
         }
     }
     
+    func testStepsAndIngredients() {
+        
+        var recipeForTesting: Recipe?
+        for recipe in store.recipes {
+            if recipe.id == "apple-pie" {
+                recipeForTesting = recipe
+                break
+            }
+        }
+        
+        let expectationResult = expectation(description: "Wait for steps to be returned from API.")
+        var stepCount = Int()
+    
+        // fetch the steps and ingredients
+        CheftyAPIClient.getStepsAndIngredients(recipe: recipeForTesting!) {
+            
+            stepCount = (recipeForTesting?.steps?.count)!
+            expectationResult.fulfill()
+            
+        }
+        waitForExpectations(timeout: 10, handler: nil)
+        XCTAssert(stepCount == 15) // apple-pie stepCount = 15
+        
+        if let recipeForTesting = recipeForTesting {
+            var steps = recipeForTesting.steps?.allObjects as! [Step]
+            steps = steps.sorted(by: { $0.timeToStart < $1.timeToStart } )
+            let ingredientsFirstStepCount = steps.first?.ingredients?.allObjects.count
+            XCTAssert(ingredientsFirstStepCount == 5) // the first step of apple pie has 5 ingredients
+        }
+    }
 }
